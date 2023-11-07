3ade1fb242c34b50eae116345f045f858367480d
MartoBozhilov <150125921+MartoBozhilov@users.noreply.github.com>
2023-11-07 14:09:00 +0200

    Spring MVC architecture

---
+++ src/main/java/com/manfashion/manfashion/ManFashionApplication.java
@@ -0,0 +1,13 @@
+package com.manfashion.manfashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManFashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManFashionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manfashion/manfashion/ManFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manfashion.manfashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManFashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
