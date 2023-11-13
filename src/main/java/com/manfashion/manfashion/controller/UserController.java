c35e321c1c2cb4f1696be1a4745f98184286977f
MartoBozhilov <150125921+MartoBozhilov@users.noreply.github.com>
2023-11-13 18:38:22 +0200

    user controller

--- src/main/java/com/manfashion/manfashion/controller/UserController.java
+++ src/main/java/com/manfashion/manfashion/controller/UserController.java
@@ -1,7 +1,14 @@
 package com.manfashion.manfashion.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+
+    @GetMapping("/users/login")
+    public String login() {
+        return "login";
+    }
+
 }
