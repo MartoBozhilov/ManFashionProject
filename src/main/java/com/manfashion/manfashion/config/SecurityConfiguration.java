563d65aa485b2fe712e2cb1c28a8379715b64db1
MartoBozhilov <150125921+MartoBozhilov@users.noreply.github.com>
2023-11-13 18:20:53 +0200

    bootstrap add

---
+++ src/main/java/com/manfashion/manfashion/config/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package com.manfashion.manfashion.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        //
+
+        return httpSecurity.build();
+    }
+
+}
