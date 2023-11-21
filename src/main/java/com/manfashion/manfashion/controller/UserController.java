851d9711866f3636a51a576dd305b4598430cdcc
MartoBozhilov <150125921+MartoBozhilov@users.noreply.github.com>
2023-11-21 17:22:09 +0200

    Update UserController.java

--- src/main/java/com/manfashion/manfashion/controller/UserController.java
+++ src/main/java/com/manfashion/manfashion/controller/UserController.java
@@ -2,13 +2,11 @@ package com.manfashion.manfashion.controller;
 
 import com.manfashion.manfashion.model.dto.UserRegistrationDTO;
 import com.manfashion.manfashion.service.UserService;
-import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -45,7 +43,7 @@ public class UserController {
 
         userService.register(userRegistrationDTO);
 
-        return "redirect:/";
+        return "redirect:/users/login";
     }
 
 }
